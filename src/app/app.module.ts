379a865fd6c965a7f9d9ef79556cb1817f9de70c
Daniel Duarte <dduarte1267@gmail.com>
2019-06-19 20:55:58 -0400

    Importación de nuevos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HouseComponent } from './house/house.component';
 import { HouseListComponent } from './house-list/house-list.component';
+import { CharacterListComponent } from './character-list/character-list.component';
+import { CharacterComponent } from './character/character.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HouseComponent,
-    HouseListComponent
+    HouseListComponent,
+    CharacterListComponent,
+    CharacterComponent
   ],
   imports: [
     BrowserModule
