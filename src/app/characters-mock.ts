871febd66d56bad805621ab038fd29e187585d47
Daniel Duarte <dduarte1267@gmail.com>
2019-06-19 20:54:56 -0400

    Mock data para Characters

---
+++ src/app/characters-mock.ts
@@ -0,0 +1,12 @@
+import { Character } from './character';
+
+export const CHARACTERS: Character[] = [
+  new Character('asjdhbasjhdbjh1hjbj123_asd', 'male', 'c1', '1982/06/11', '2000/09/21', '2', 'Dummy alias', 'Dummy Father', 'Dummy mother', 'sp', 'all'),
+  new Character('asjdhbasjhdbjh1hjbj123_asd', 'male', 'c1', '1982/06/11', '2000/09/21', '2', 'Dummy alias', 'Dummy Father', 'Dummy mother', 'sp', 'all'),
+  new Character('asjdhbasjhdbjh1hjbj123_asd', 'male', 'c1', '1982/06/11', '2000/09/21', '2', 'Dummy alias', 'Dummy Father', 'Dummy mother', 'sp', 'all'),
+  new Character('asjdhbasjhdbjh1hjbj123_asd', 'male', 'c1', '1982/06/11', '2000/09/21', '2', 'Dummy alias', 'Dummy Father', 'Dummy mother', 'sp', 'all'),
+  new Character('asjdhbasjhdbjh1hjbj123_asd', 'male', 'c1', '1982/06/11', '2000/09/21', '2', 'Dummy alias', 'Dummy Father', 'Dummy mother', 'sp', 'all'),
+  new Character('asjdhbasjhdbjh1hjbj123_asd', 'male', 'c1', '1982/06/11', '2000/09/21', '2', 'Dummy alias', 'Dummy Father', 'Dummy mother', 'sp', 'all'),
+  new Character('asjdhbasjhdbjh1hjbj123_asd', 'male', 'c1', '1982/06/11', '2000/09/21', '2', 'Dummy alias', 'Dummy Father', 'Dummy mother', 'sp', 'all'),
+  new Character('asjdhbasjhdbjh1hjbj123_asd', 'male', 'c1', '1982/06/11', '2000/09/21', '2', 'Dummy alias', 'Dummy Father', 'Dummy mother', 'sp', 'all')
+];
