5d69919da0c9248c2dee2ddb35b040451ae10c83
Daniel Duarte <dduarte1267@gmail.com>
2019-06-24 12:14:55 -0400

    Corrección de response desde capa de servicios

--- code/src/app/rest.service.ts
+++ code/src/app/rest.service.ts
@@ -11,10 +11,10 @@ export class RestService {
   constructor(private http:HttpClient) { }
 
   public getHouses () {
-    return this.http.get(this.baseUrl + '/houses');
+    return this.http.get<any[]>(this.baseUrl + '/houses');
   }
 
   public getCharacters () {
-    return this.http.get(this.baseUrl + '/characters');
+    return this.http.get<any[]>(this.baseUrl + '/characters');
   }
 }
